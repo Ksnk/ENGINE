fd052d3e8c115bae91de5f9d010e43d0c4ef139e
ksnk <sergekoriakin@gmail.com>
2018-06-09 15:22:18 +0300

    1.0.1

---
+++ src/kernel/options/engine_options_session.php
@@ -0,0 +1,27 @@
+/**
+ * класс для хранения параметров в сессии
+ */
+/* <%=POINT::get('ENGINE_namespace') %> */
+
+class engine_options_session implements engine_options
+{
+
+    function get($name)
+    {
+        if (ENGINE::$session_started)
+            return $_SESSION[$name];
+        else
+            return null;
+    }
+
+
+    function set($name, $value = null)
+    {
+        ENGINE::start_session();
+        if (empty($value))
+            unset($_SESSION[$name]);
+        else
+            $_SESSION[$name] = $value;
+    }
+
+}
